662d202045d8acbb60cbc1030ee7071d556c2795
tjhooper1 <tomh386@gmail.com>
2020-02-17 22:07:52 -0500

    made a quick server to host files

---
+++ http-response/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+type hotdog int
+
+func (m hotdog) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	err := req.ParseForm()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	tpl.ExecuteTemplate(w, "index.gohtml", req.Form)
+	fmt.Println("hey")
+}
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("index.gohtml"))
+}
+
+func main() {
+	var d hotdog
+	http.ListenAndServe(":8080", d)
+}
